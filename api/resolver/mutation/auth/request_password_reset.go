790a5e0b693a7777a732901cb248f0195c9a4ae3
talentdev0001 <one.incredy@gmail.com>
2023-07-27 11:22:53 +0500

    feat(verification, reset): added email validation

--- api/resolver/mutation/auth/request_password_reset.go
+++ api/resolver/mutation/auth/request_password_reset.go
@@ -4,12 +4,20 @@ import (
 	"context"
 
 	"github.com/steebchen/keskin-api/gqlgen"
+	"github.com/steebchen/keskin-api/gqlgen/gqlerrors"
+	"github.com/steebchen/keskin-api/lib/validator"
 )
 
 func (a *Auth) RequestPasswordReset(
 	ctx context.Context,
 	input gqlgen.RequestPasswordResetInput,
 ) (*gqlgen.RequestPasswordResetPayload, error) {
+
+	err := validator.Email(input.Email)
+	if err != nil {
+		return nil, gqlerrors.NewValidationError(err.Error(), "InvalidEmail")
+	}
+
 	RequestPasswordReset(a.Prisma, ctx, input.Email, input.Company)
 
 	return &gqlgen.RequestPasswordResetPayload{
