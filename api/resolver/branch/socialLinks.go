14f54ab56b538b3988d5848c118161a10f260129
talentdev0001 <one.incredy@gmail.com>
2023-07-31 11:14:18 +0500

    feat(branch): added social media links in branch schema, create and update mutation

---
+++ api/resolver/branch/socialLinks.go
@@ -0,0 +1,13 @@
+package branch
+
+// func FacebookLink(ctx context.Context, obj *prisma.Branch) (*string, error) {
+// 	return nil, nil
+// }
+
+// func TiktokLink(ctx context.Context, obj *prisma.Branch) (*string, error) {
+// 	return nil, nil
+// }
+
+// func InstagramLink(ctx context.Context, obj *prisma.Branch) (*string, error) {
+// 	return nil, nil
+// }
